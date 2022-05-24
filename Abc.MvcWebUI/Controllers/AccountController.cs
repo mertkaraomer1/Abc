73c7ed3345ca4b4d38be3b97cc221af825b8ebd5
Lenovo <Lenovo@DESKTOP-AVC9VTM>
2022-05-25 01:00:13 +0300

    devam

---
+++ Abc.MvcWebUI/Controllers/AccountController.cs
@@ -0,0 +1,63 @@
+﻿using Abc.MvcWebUI.Identity;
+using Abc.MvcWebUI.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Abc.MvcWebUI.Controllers
+{
+    public class AccountController : Controller
+    {
+        private UserManager<AplicationUser> UserManager;
+        private RoleManager<AplicationRole> RoleManager;
+        public AccountController()
+        {
+            var userStore = new UserStore<AplicationUser>(new IdentityDataContext());
+            UserManager = new UserManager<AplicationUser>(userStore);
+            var roleStore = new RoleStore<AplicationRole>(new IdentityDataContext());
+            RoleManager = new RoleManager<AplicationRole>(roleStore);
+        }
+        // GET: Account
+        public ActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Register(Register model)
+        {
+            var user = new AplicationUser();
+            user.Name = model.Name;
+            user.Surname = model.SurName;
+            user.Email = model.Email;
+            user.UserName = model.UserName;
+
+            IdentityResult result = UserManager.Create(user, model.Password);
+            if(result.Succeeded)
+            {
+                //kullanıcı oluştu ve kullanıcıya bir role atayabilirsiniz.
+                if(RoleManager.RoleExists("User"))
+                {
+                    UserManager.AddToRole(user.Id, "User");
+                }
+                return RedirectToAction("Login", "Account");
+
+            }
+            else
+            {
+                ModelState.AddModelError("RegisterUserError", "Kullanıcı oluşturma hatası.");
+            }
+
+
+
+
+
+
+            return View();
+        }
+    }
+}
---
+++ Abc.MvcWebUI/Identity/AplicationRole.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Abc.MvcWebUI.Identity
+{
+    public class AplicationRole:IdentityRole
+    {
+        public string Description { get; set; }
+        public AplicationRole()
+        {
+
+        }
+        public AplicationRole(string rolename,string description)
+        {
+            this.Description = description;
+        }
+    }
+}
